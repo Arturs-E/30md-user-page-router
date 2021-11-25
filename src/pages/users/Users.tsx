bdb8a5e2e181596a6025266dd16af59184af538a
Arturs Endzelins <arturs.endzelins@gmail.com>
2021-11-25 15:44:34 +0200

    Added searchParams

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,34 +1,80 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import './Users.scss';
+import { Link, useSearchParams } from 'react-router-dom';
 import users from '../../data/users.json';
 
-const Users = () => (
-  <section className="users-wrapper">
-    <div className="users">
-      <table className="result-table">
-        <tr>
-          <th>User ID</th>
-          <th>Name</th>
+const Users = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const user = searchParams.get('user');
+  const [inputValue, setInputValue] = useState('');
+
+  const loadAllUsers = () => (
+    users.map((item) => (
+      <tr key={item.id}>
+        <td>
+          <Link to={`${item.id}`}>
+            {item.id}
+          </Link>
+        </td>
+        <td>
+          <Link to={`${item.id}`}>
+            {item.name}
+          </Link>
+        </td>
+      </tr>
+    ))
+  );
+
+  const loadFilteredUsers = () => (
+    users
+      .filter((value) => value.name.toLowerCase().includes(user!.toLowerCase()))
+      .map((item) => (
+        <tr key={item.id}>
+          <td>
+            <Link to={`${item.id}`}>
+              {item.id}
+            </Link>
+          </td>
+          <td>
+            <Link to={`${item.id}`}>
+              {item.name}
+            </Link>
+          </td>
         </tr>
-        {
-          users.map((user) => (
-            <tr key={user.id}>
-              <td>
-                <Link to={`${user.id}`}>
-                  {user.id}
-                </Link>
-              </td>
-              <td>
-                <Link to={`${user.id}`}>
-                  {user.name}
-                </Link>
-              </td>
-            </tr>
-          ))
-        }
-      </table>
-    </div>
-  </section>
-);
+      ))
+  );
+
+  return (
+    <section className="users-wrapper">
+      <div>
+        <form onSubmit={(event) => {
+          event.preventDefault();
+          setSearchParams({ user: inputValue });
+        }}
+        >
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          />
+          <button type="submit">Search Users</button>
+        </form>
+      </div>
+      <div className="users">
+        <table className="result-table">
+          <tr>
+            <th>User ID</th>
+            <th>Name</th>
+          </tr>
+          {
+            user === null
+              ? (loadAllUsers())
+              : (loadFilteredUsers())
+          }
+        </table>
+      </div>
+    </section>
+  );
+};
 
 export default Users;
